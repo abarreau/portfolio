f5b91c03acd86ed9e866818b2d1e157a0216ecc8
Anthony <anthony@naturalpad.fr>
2017-03-29 22:24:03 +0200

    [fix] yolo.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,56 @@
+'use strict';
+/**
+ * Handles the main visual effects of the page
+ */
 $(document).ready(function() {
+    var navbarHeight            = 100;
+    var navbarCollapseButton    = $("#navbar-collapse-button");
+    var navbar                  = $("#navbar");
+
+    /**
+     * On click on a navbar link, we jump straight to the corresponding section
+     */
     $(".navbar-link").click(function(e) {
         e.preventDefault();
         var target = $(this).attr("href");
         var position = 0;
 
-        if(target !== "#top") {
-            position = $(target).offset().top - 100;
+        if (target !== "#top") {
+            position = $(target).offset().top - navbarHeight;
         }
 
         $("html, body").animate({
             scrollTop: position
         }, 1000);
+
+        // in case we're in small device, i.e. navbar is folded
+        // we automatically close the navbar
+        if ($(window).width() <= 768) {
+            navbarCollapseButton.removeClass("collapsed");
+            navbar.removeClass("in");
+        }
     });
 
+    /**
+     * Listener on scroll page event.
+     * Changes the navbar style on scroll
+     */
     $(window).scroll(function() {
-        if($(this).scrollTop() == 0) {
+        console.log($(this).scrollTop());
+        
+        if ($(this).scrollTop() == 0) {
             $("#navbar").removeClass("reduced");
         } else {
             $("#navbar").addClass("reduced");
         }
     });
 
-    $("#abstract").css("height", $(window).height());
+    /**
+     * Sets the first section to the height of the viewport
+     */
+    $("#abstract").css("min-height", $(window).height());
+    
+    $( window ).resize(function() {
+        $("#abstract").css("min-height", $(window).height());
+    });
 });
